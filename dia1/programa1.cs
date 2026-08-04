4ae3da8b36666f269cfda36abab07aaf2f8ee249
Alberto Escareno <jaesalberto2@gmail.com>
2026-08-04 16:34:43 -0600

    Add user input functionality to greet by name

--- dia1/programa1.cs
+++ dia1/programa1.cs
@@ -5,5 +5,9 @@ class Program
     static void Main()
     {
         Console.WriteLine("¡Hola desde C#!");
+
+        Console.WriteLine("Ingrese su nombre:");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("¡Hola, " + nombre + "!");
     }
 }
